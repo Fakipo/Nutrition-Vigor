9126578281804f9c0385083cc4bf64055b620f3e
Fakipo <paraskavdikar@gmail.com>
2025-01-03 21:47:30 -0600

    Initializing project

---
+++ food_and_health/src/main/java/nutrition/vigor/foodandhealth/FoodandhealthApplication.java
@@ -0,0 +1,13 @@
+package nutrition.vigor.foodandhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodandhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodandhealthApplication.class, args);
+		System.out.println("here we are starting up");	}
+
+}
